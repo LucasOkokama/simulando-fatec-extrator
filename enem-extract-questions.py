e82fbee04ecef5b209fd9c7bfbd86aa403be29b0
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-10-28 21:36:43 -0300

    feat(enem-extract-questions): move img para diretorio correto e implementa cooldown de requisição

--- enem-extract-questions.py
+++ enem-extract-questions.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+import time
 
 # Variaveis de Configuração
 __anoDaProva = 2023
@@ -10,14 +11,18 @@ __anoDaProva = 2023
 url = f"https://api.enem.dev/v1/exams/{__anoDaProva}/questions/"
 # Diretorio para onde as questoes irão
 dir_questions = "vestibulares/enem"
+# Diretorio para onde as imagens irão
+dir_images = f"vestibulares/enemFormatado/{__anoDaProva}"
 
 # Cria o diretório se ele não existir
 os.makedirs(dir_questions, exist_ok=True)
+os.makedirs(dir_images, exist_ok=True)
 
 # Número da ultima questões a ser extraida
-num_questions = 33
+num_questions = 180
 
-for i in range(24, num_questions + 1):
+for i in range(1, num_questions + 1):
+
     # Modifica a URL para buscar a questão com índice específico
     response = requests.get(f"{url}{i}")
 
@@ -53,7 +58,7 @@ for i in range(24, num_questions + 1):
                 # Se a requisição deu certo (code 200)
                 if img_response.status_code == 200:
                     # Cria o path da imagem levando em consideração o index da img
-                    img_path = os.path.join(dir_questions, f"img_{img_index}_enem_{i}.png")
+                    img_path = os.path.join(dir_images, f"img_{img_index}_enem_{i}.png")
 
                     # Salva o conteúdo no path/arquivo.extensão com nome definido acima
                     with open(img_path, "wb") as img_path:
@@ -68,4 +73,12 @@ for i in range(24, num_questions + 1):
         print(f"Falha ao obter a questão {i}: Status_Code {response.status_code}")
 
 
+    # Espaçamento para facilitar leitura do console
     print("\n")
+
+
+
+    # Espera 2 segundos a cada 10 iterações
+    if i % 10 == 0:
+        print("\n\nAGUARDA 7 SEGUNDOS PARA NÃO ESTOURAR O LIMITE DE REQUISIÇÕES!\n\n\n")
+        time.sleep(7)
