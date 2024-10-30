77580af9096d2e1eaf904496935b0f8eba139a2a
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-10-30 16:06:34 -0300

    feat(enem-format-questions): cria a estrutura json formatada e com alguns valores

--- enem-format-questions.py
+++ enem-format-questions.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 
 
 # Variáveis de Configuração
@@ -15,15 +16,28 @@ os.makedirs(dir_questions_txt, exist_ok=True)
 os.makedirs(dir_questions_json, exist_ok=True)
 
 # Número da última questões a ser extraida
-num_questions = 5
+num_questions = 110
 
-for i in range(1, num_questions + 1):
+for i in range(110, num_questions + 1):
     path_questions = os.path.join(dir_questions_txt, f"enem_questao{i}.txt")
 
     try:
         with open(path_questions, 'r') as path_questions:
             question_json =  json.load(path_questions)
 
+            question_json_formatted = {
+                "vestibular": 1,
+                "ano": question_json['year'],
+                "num_questao": question_json['index'],
+                "disciplina": question_json['discipline'],
+                "enunciado": question_json['context'],
+                "imgs": [],
+                "pergunta": question_json['alternativesIntroduction'],
+                "gabarito": question_json['correctAlternative'],
+                "alternativas": []
+            }
+
+
     except json.JSONDecodeError as error:
-        print("O conteúdo do arquivo não está em um formato JSON válido.\n", error);
-        question_json = None;
+        print("O conteúdo do arquivo não está em um formato JSON válido.\n", error)
+        question_json = None
