0cf0c047288426ad99b1bf8bf2f94ba0ff1cb25d
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-10-30 12:51:56 -0300

    fix(enem-format-questions): arruma nome dos paths dos dados

--- enem-format-questions.py
+++ enem-format-questions.py
@@ -6,19 +6,19 @@ import os
 __anoDaProva = 2023
 
 # Diretório para acessar as questões NÃO FORMATADAS
-dir_questions = "vestibulares/enem"
+dir_questions_txt = "vestibulares/enem"
 # Diretório para onde as questões FORMATADAS irão
-dir_images = f"vestibulares/enemFormatado/{__anoDaProva}"
+dir_questions_json = f"vestibulares/enemFormatado/{__anoDaProva}"
 
 # Cria o diretório se ele não existir
-os.makedirs(dir_questions, exist_ok=True)
-os.makedirs(dir_images, exist_ok=True)
+os.makedirs(dir_questions_txt, exist_ok=True)
+os.makedirs(dir_questions_json, exist_ok=True)
 
 # Número da última questões a ser extraida
 num_questions = 5
 
 for i in range(1, num_questions + 1):
-    path_questions = os.path.join(dir_questions, f"enem_questao{i}.txt")
+    path_questions = os.path.join(dir_questions_txt, f"enem_questao{i}.txt")
 
     with open(path_questions, 'r') as path_questions:
         question_txt = path_questions.read();
