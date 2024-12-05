b9771eb965995b33d4bb97bbf8595da985e25feb
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-12-04 23:46:17 -0300

    feat(fatec-format-questions): adiciona tratamento de erro para a questão

--- vestibulares/fatec/fatec_format_questions.py
+++ vestibulares/fatec/fatec_format_questions.py
@@ -73,83 +73,89 @@ def fatecFormatarQuestoes(anoDaProva, semestreDaProva):
 
 
       for num_questao in range(1, 55):
-        # Preenche um JSON com os valores de uma determinada questão
-        prompt_modelo = f"Faça tudo isso utilizando como base a questão de número {num_questao:02}\n\n"
-        prompt_modelo += prompt_pedido
-        prompt_modelo += prompt_json_modelo
-        response_JSON = model.generate_content([prompt_modelo, files[0]])
-        print(f"> Criando JSON da questao {num_questao}")
-        # Retorna o custo de Token da requisição
-        print(response_JSON.usage_metadata)
+        try:
+          # Preenche um JSON com os valores de uma determinada questão
+          prompt_modelo = f"Faça tudo isso utilizando como base a questão de número {num_questao:02}\n\n"
+          prompt_modelo += prompt_pedido
+          prompt_modelo += prompt_json_modelo
+          response_JSON = model.generate_content([prompt_modelo, files[0]])
+          print(f"> Criando JSON da questao {num_questao} ({anoDaProva}/{semestreDaProva})")
+          # Retorna o custo de Token da requisição
+          print(response_JSON.usage_metadata)
 
-        # Retorna o texto de suporte de uma determinada questão
-        prompt_modelo = f"Faça isso para a questão {num_questao:02}\n\n"
-        prompt_modelo += prompt_enunciadoExtra
-        response_EnunciadoExtra = model.generate_content([prompt_modelo, files[0]])
-        print(f"> Identificando o texto de suporte da questao {num_questao}")
-        # Retorna o custo de Token da requisição
-        print(response_EnunciadoExtra.usage_metadata)
+          # Retorna o texto de suporte de uma determinada questão
+          prompt_modelo = f"Faça isso para a questão {num_questao:02}\n\n"
+          prompt_modelo += prompt_enunciadoExtra
+          response_EnunciadoExtra = model.generate_content([prompt_modelo, files[0]])
+          print(f"> Identificando o texto de suporte da questao {num_questao} ({anoDaProva}/{semestreDaProva})")
+          # Retorna o custo de Token da requisição
+          print(response_EnunciadoExtra.usage_metadata)
 
-        # Retorna somente a disciplina de uma determinada questão
-        prompt_Disciplina = f"Me retorne somente a disciplina da questão {num_questao:03}"
-        response_Disciplina = model.generate_content([prompt_Disciplina, files[1]])
-        print(f"> Identificando disciplina da questao {num_questao}")
-        # Retorna o custo de Token da requisição
-        print(response_Disciplina.usage_metadata)
+          # Retorna somente a disciplina de uma determinada questão
+          prompt_Disciplina = f"Me retorne somente a disciplina da questão {num_questao:03}"
+          response_Disciplina = model.generate_content([prompt_Disciplina, files[1]])
+          print(f"> Identificando disciplina da questao {num_questao} ({anoDaProva}/{semestreDaProva})")
+          # Retorna o custo de Token da requisição
+          print(response_Disciplina.usage_metadata)
 
-        # Retorna somente o gabarito de uma determinada questão
-        prompt_Gabarito = f"Me retorne somente o gabarito da questão {num_questao:03}"
-        response_Gabarito = model.generate_content([prompt_Gabarito, files[1]])
-        print(f"> Identificando gabarito da questao {num_questao}")
-        # Retorna o custo de Token da requisição
-        print(response_Gabarito.usage_metadata)
-        print("\n")
-
-
-        # Remove as marcações do "fenced code block" (aspas triplas)
-        questao_formatada_json = response_JSON.text.replace('```json', '').replace('```', '')
+          # Retorna somente o gabarito de uma determinada questão
+          prompt_Gabarito = f"Me retorne somente o gabarito da questão {num_questao:03}"
+          response_Gabarito = model.generate_content([prompt_Gabarito, files[1]])
+          print(f"> Identificando gabarito da questao {num_questao} ({anoDaProva}/{semestreDaProva})")
+          # Retorna o custo de Token da requisição
+          print(response_Gabarito.usage_metadata)
+          print("\n")
 
 
-        # Passa de string (com formatação JSON) para um dicionario do Python
-        questao_formatada_json = json.loads(questao_formatada_json)
-
-        # Adiciona valores à alguns campos
-        questao_formatada_json['ano'] = anoDaProva
-        questao_formatada_json['vestibular'] = 2
-        questao_formatada_json['semestre'] = semestreDaProva
-        questao_formatada_json['disciplina'] = response_Disciplina.text
-        questao_formatada_json['gabarito'] = response_Gabarito.text
-
-        if response_EnunciadoExtra.text != "Nao_Existe":
-          backup_enunciado = questao_formatada_json['enunciado']
-          questao_formatada_json['enunciado'] = response_EnunciadoExtra.text
-          if backup_enunciado is not None:
-            questao_formatada_json['enunciado'] += "\n\n"
-            questao_formatada_json['enunciado'] += backup_enunciado
+          # Remove as marcações do "fenced code block" (aspas triplas)
+          questao_formatada_json = response_JSON.text.replace('```json', '').replace('```', '')
 
 
-        if(questao_formatada_json['enunciado'] is not None):
-          # Substitui os "\n" por "<br>"
-          questao_formatada_json['enunciado'] = questao_formatada_json['enunciado'].replace('\n', '<br>')
+          # Passa de string (com formatação JSON) para um dicionario do Python
+          questao_formatada_json = json.loads(questao_formatada_json)
 
-        if(questao_formatada_json['pergunta'] is not None):
-          # Substitui os "\n" por "<br>"
-          questao_formatada_json['pergunta'] = questao_formatada_json['pergunta'].replace('\n', '<br>')
+          # Adiciona valores à alguns campos
+          questao_formatada_json['ano'] = anoDaProva
+          questao_formatada_json['vestibular'] = 2
+          questao_formatada_json['semestre'] = semestreDaProva
+          questao_formatada_json['disciplina'] = response_Disciplina.text
+          questao_formatada_json['gabarito'] = response_Gabarito.text
 
-
-        # # Salva o caminho para onde a questão formatada deve ir
-        dir_questions_number_json = os.path.join(dir_questions_json, str(num_questao))
-        # # Cria esse caminho caso ele não exista
-        os.makedirs(dir_questions_number_json, exist_ok=True)
-
-        # # Salva o arquivo json no caminho especificado
-        path_json_formatted = os.path.join(dir_questions_number_json, f"fatec_questao{num_questao}.json")
-        with open(path_json_formatted, 'w') as path_json_formatted:
-          # Passa o questao_formatada_json para JSON e salvar em um arquivo
-          json.dump(questao_formatada_json, path_json_formatted, indent=4, ensure_ascii=False)
+          if response_EnunciadoExtra.text != "Nao_Existe":
+            backup_enunciado = questao_formatada_json['enunciado']
+            questao_formatada_json['enunciado'] = response_EnunciadoExtra.text
+            if backup_enunciado is not None:
+              questao_formatada_json['enunciado'] += "\n\n"
+              questao_formatada_json['enunciado'] += backup_enunciado
 
 
-        time.sleep(3)
+          if(questao_formatada_json['enunciado'] is not None):
+            # Substitui os "\n" por "<br>"
+            questao_formatada_json['enunciado'] = questao_formatada_json['enunciado'].replace('\n', '<br>')
+
+          if(questao_formatada_json['pergunta'] is not None):
+            # Substitui os "\n" por "<br>"
+            questao_formatada_json['pergunta'] = questao_formatada_json['pergunta'].replace('\n', '<br>')
+
+
+          # # Salva o caminho para onde a questão formatada deve ir
+          dir_questions_number_json = os.path.join(dir_questions_json, str(num_questao))
+          # # Cria esse caminho caso ele não exista
+          os.makedirs(dir_questions_number_json, exist_ok=True)
+
+          # # Salva o arquivo json no caminho especificado
+          path_json_formatted = os.path.join(dir_questions_number_json, f"fatec_questao{num_questao}.json")
+          with open(path_json_formatted, 'w') as path_json_formatted:
+            # Passa o questao_formatada_json para JSON e salvar em um arquivo
+            json.dump(questao_formatada_json, path_json_formatted, indent=4, ensure_ascii=False)
+
+
+          time.sleep(3)
+
+        except Exception as e:
+          # Mostra o erro e permite continuação da execução
+          print(f">>> Erro ao processar a questão {num_questao}: {e}")
+          time.sleep(3)
 
   except FileNotFoundError:
     # Tratamento para arquivo não encontrado
