1350ea8a7ee2c09efefc966cf1cd8d4ed7ea409d
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-12-04 21:49:33 -0300

    feat(fatecpy): cria loop incluindo semestre

--- fatec.py
+++ fatec.py
@@ -1,4 +1,11 @@
 from vestibulares.fatec.fatec_format_questions import fatecFormatarQuestoes
 
-for anoDaProva in range(2024, 2023, -1):
-    fatecFormatarQuestoes(anoDaProva)
+import os
+
+for anoDaProva in range(2024, 2008, -1):
+    for semestreDaProva in range(1, 3):
+        prova_path = f"vestibulares/fatec/pdf/fatecProva_{anoDaProva}_{semestreDaProva}.pdf"
+        gabarito_path = f"vestibulares/fatec/pdf/fatecGabarito_{anoDaProva}_{semestreDaProva}.pdf"
+
+        if os.path.isfile(prova_path) and os.path.isfile(gabarito_path):
+            fatecFormatarQuestoes(anoDaProva, semestreDaProva)
