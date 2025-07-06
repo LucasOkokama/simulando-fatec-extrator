f5a8197f1fb125c52573fe49955df5210b78f339
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-07-06 20:53:36 -0300

    refactor: melhora a otimizacao do codigo

--- fatec_format_questions.py
+++ fatec_format_questions.py
@@ -1,19 +1,14 @@
-
-# Executar o seguinte comando:
-# pip install -q -U google-genai
 from google import genai
 from google.genai import types
-
-import pathlib
 from dotenv import load_dotenv
+import pathlib
 import json
-import re
 import os
 import time
 
-
 # Carrega .env
 load_dotenv()
+
 
 def fatecFormatarQuestoes(anoDaProva, semestreDaProva, provaPath, gabaritoPath):
   try:
@@ -24,10 +19,6 @@ def fatecFormatarQuestoes(anoDaProva, semestreDaProva, provaPath, gabaritoPath):
     geminiai_key = os.getenv("GEMINIAI_KEY")
     client = genai.Client(api_key=geminiai_key)
 
-    # Diretório da pasta da prova
-    dir_prova = f"fatec_formatado/{anoDaProva}_{semestreDaProva}"
-    os.makedirs(dir_prova, exist_ok=True)
-
 
 
     # Array contendo os arquivos (PDF's)
@@ -37,17 +28,12 @@ def fatecFormatarQuestoes(anoDaProva, semestreDaProva, provaPath, gabaritoPath):
     ]
 
     # Armazena o PROMPT do Enunciado, Pergunta e Alternativas
-    with open('prompt/prompt-enunciado-pergunta-alternativas.txt', 'r', encoding="utf-8") as my_file:
-      prompt_enunciado_pergunta_alternativa = my_file.read()
-
-    # Armazena o PROMPT do Enunciado extra
-    with open('prompt/prompt-enunciadoExtra.txt', 'r', encoding="utf-8") as my_file:
-      prompt_enunciadoExtra = my_file.read()
+    with open('prompt/geminiai-prompt.txt', 'r', encoding="utf-8") as my_file:
+      prompt_questao = my_file.read()
 
     # Armazena o PROMPT do JSON base
-    with open('prompt/question-formatted.json', 'r', encoding="utf-8") as my_file:
+    with open('prompt/questao-base-prompt.json', 'r', encoding="utf-8") as my_file:
       prompt_jsonBase = json.load(my_file)
-
     # Transforma o JSON em uma String
     prompt_jsonBase_string = json.dumps(prompt_jsonBase)
 
@@ -59,7 +45,7 @@ def fatecFormatarQuestoes(anoDaProva, semestreDaProva, provaPath, gabaritoPath):
         # Retorna o Enunciado, Pergunta e Alternativa
         print(f"> Criando JSON da questao {num_questao} ({anoDaProva}/{semestreDaProva})")
         prompt = f"Faça tudo isso utilizando como base a questão de número {num_questao:02}\n\n"
-        prompt += prompt_enunciado_pergunta_alternativa
+        prompt += prompt_questao
         prompt += prompt_jsonBase_string
         response = client.models.generate_content(
           model=geminiai_model,
@@ -68,124 +54,53 @@ def fatecFormatarQuestoes(anoDaProva, semestreDaProva, provaPath, gabaritoPath):
             types.Part.from_bytes(
               data=files[0].read_bytes(),
               mime_type='application/pdf',
-            )
-          ]
-        )
-        print(f"{response.usage_metadata.total_token_count} tokens usados")
-
-
-        # Retorna o Enunciado extra (texto de suporte)
-        print(f"> Identificando o texto de suporte da questao {num_questao} ({anoDaProva}/{semestreDaProva})")
-        prompt = f"Faça isso para a questão {num_questao:02}\n\n"
-        prompt += prompt_enunciadoExtra
-        response_EnunciadoExtra = client.models.generate_content(
-          model=geminiai_model,
-          contents=[
-            prompt,
-            types.Part.from_bytes(
-              data=files[0].read_bytes(),
-              mime_type='application/pdf',
-            )
-          ]
-        )
-        print(f"{response_EnunciadoExtra.usage_metadata.total_token_count} tokens usados")
-
-
-        # Retorna somente a disciplina de uma determinada questão
-        print(f"> Identificando disciplina da questao {num_questao} ({anoDaProva}/{semestreDaProva})")
-        prompt_Disciplina = (f"Me retorne somente a disciplina da questão {num_questao:03}, sendo ela representado somente por "
-                             f"Matemática, Português, Física, Química, Biologia, História, Geografia, Inglês, Raciocínio Lógico ou Multidisciplinar")
-        response_Disciplina = client.models.generate_content(
-          model=geminiai_model,
-          contents=[
-            prompt_Disciplina,
+            ),
             types.Part.from_bytes(
               data=files[1].read_bytes(),
               mime_type='application/pdf',
             )
           ]
         )
-        print(f"{response_Disciplina.usage_metadata.total_token_count} tokens usados")
-
-
-        # Retorna somente o gabarito de uma determinada questão
-        print(f"> Identificando gabarito da questao {num_questao} ({anoDaProva}/{semestreDaProva})")
-        prompt_Gabarito = f"Me retorne somente o gabarito da questão {num_questao:03}, sendo ele representado somente por A, B, C, D ou E"
-        response_Gabarito = client.models.generate_content(
-          model=geminiai_model,
-          contents=[
-            prompt_Gabarito,
-            types.Part.from_bytes(
-              data=files[1].read_bytes(),
-              mime_type='application/pdf',
-            )
-          ]
-        )
-        print(f"{response_Gabarito.usage_metadata.total_token_count} tokens usados")
-        print("\n")
+        print(f"{response.usage_metadata.total_token_count} tokens usados\n")
 
 
 
         # Remove as marcações do "fenced code block" (aspas triplas)
-        questao_formatada_json = response.text.replace('```json', '').replace('```', '')
+        questao_formatada = response.text.replace('```json', '').replace('```', '')
 
         # Passa de string (com formatação JSON) para um dicionario do Python
-        questao_formatada_json = json.loads(questao_formatada_json)
-
-
+        questao_formatada = json.loads(questao_formatada)
 
         # Adiciona valores à alguns campos
-        questao_formatada_json['prova']['id']['ano'] = anoDaProva
-        questao_formatada_json['prova']['id']['semestre'] = semestreDaProva
-        questao_formatada_json['disciplina'] = response_Disciplina.text
-        questao_formatada_json['gabarito'] = response_Gabarito.text
-
-        if response_EnunciadoExtra.text != "Nao_Existe":
-          backup_enunciado = questao_formatada_json['enunciado']
-          questao_formatada_json['enunciado'] = response_EnunciadoExtra.text
-          if (backup_enunciado is not None) and (backup_enunciado != response_EnunciadoExtra.text):
-            questao_formatada_json['enunciado'] += "\n\n"
-            questao_formatada_json['enunciado'] += backup_enunciado
+        questao_formatada['prova']['id']['ano'] = anoDaProva
+        questao_formatada['prova']['id']['semestre'] = semestreDaProva
+        questao_formatada['numQuestao'] = num_questao
 
 
 
-        # Substitui "\n" por "<br>"
-        if(questao_formatada_json['enunciado'] is not None):
-          # Substitui os "\n" por "<br>"
-          questao_formatada_json['enunciado'] = questao_formatada_json['enunciado'].replace('\n', '<br>')
+        # Diretório onde o arquivo será salvo
+        dir_prova_questao = f"fatec_formatado/{anoDaProva}_{semestreDaProva}/{num_questao}"
+        os.makedirs(dir_prova_questao, exist_ok=True)
 
-        if(questao_formatada_json['pergunta'] is not None):
-          # Substitui os "\n" por "<br>"
-          questao_formatada_json['pergunta'] = questao_formatada_json['pergunta'].replace('\n', '<br>')
-
-
-
-        # Diretório para onde as questões formatada devem ir
-        dir_questao_numero_json = os.path.join(dir_prova, str(num_questao))
-        os.makedirs(dir_questao_numero_json, exist_ok=True)
-
-        # Salva o arquivo json no caminho especificado
-        dir_final = os.path.join(dir_questao_numero_json, f"fatec_questao{num_questao}.json")
-        with open(dir_final, 'w', encoding='utf-8') as dir_final:
-          # Passa o questao_formatada_json para JSON e salvar em um arquivo
-          json.dump(questao_formatada_json, dir_final, indent=4, ensure_ascii=False)
+        # Salva o arquivo JSON no caminho especificado
+        dir_final = os.path.join(dir_prova_questao, f"fatec_questao{num_questao}.json")
+        with open(dir_final, 'w', encoding='utf-8') as file:
+          json.dump(questao_formatada, file, indent=4, ensure_ascii=False)
 
 
-        time.sleep(5)
+
+        time.sleep(6)
 
       except Exception as e:
         # Mostra o erro e permite continuação da execução
         print(f">>> Erro ao processar a questão {num_questao}: {e}<<<\n\n\n")
-        time.sleep(5)
+        time.sleep(6)
 
   except FileNotFoundError:
-    # Tratamento para arquivo não encontrado
     print("Erro: O arquivo não foi encontrado.")
 
   except PermissionError:
-    # Tratamento para erro de permissão
     print("Erro: Permissão negada para abrir o arquivo.")
 
   except Exception as e:
-    # Tratamento genérico para outros erros
     print(f"Erro inesperado: {e}")
